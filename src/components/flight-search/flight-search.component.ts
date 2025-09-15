721c4abded9178016c60bca1c178ecf3c4286099
bacemakkari <Benakkari.bacem@tekboot.com>
2025-09-15 14:05:27 +0200

    Updated flight-search.component.ts

--- src/components/flight-search/flight-search.component.ts
+++ src/components/flight-search/flight-search.component.ts
@@ -5,10 +5,11 @@
 +      this.errorMessage.set('Veuillez remplir au minimum les champs départ, arrivée et date de départ.');
        return;
      }
+   }
 
-      const prevDuration = typeof prev.temps_trajet === 'number' ? prev.temps_trajet : 0;
-      const currentDuration = typeof current.temps_trajet === 'number' ? current.temps_trajet : 0;
+@@ .. @@
+   isSearchValid(): boolean {
      const params = this.searchParams();
 -    return !!params.ville_depart && !!params.ville_arrivee && !!params.date_depart && !!params.date_retour;
-    return shortest.temps_trajet_formatted || '0:00';
++    return !!params.ville_depart && !!params.ville_arrivee && !!params.date_depart;
    }
